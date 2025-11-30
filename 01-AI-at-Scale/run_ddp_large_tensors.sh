c5b753a3d51ba4b4712ef01dcbe6d44318a00eef
Clare Xie <clarexie@g.harvard.edu>
2025-11-30 07:45:55 +0000

    Add tensor dataset files and ddp_large_tensors job script

---
+++ 01-AI-at-Scale/run_ddp_large_tensors.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+NNODES=`wc -l < $PBS_NODEFILE`
+NRANKS_PER_NODE=4
+let NRANKS=$((NNODES*NRANKS_PER_NODE))
+
+module use /soft/modulefiles
+module load conda/2025-09-25
+conda activate
+
+export DISABLE_PYMODULE_LOG=1
+export CPU_AFFINITY="verbose,list:0,1:8,9:16,17:24,25"
+
+mpiexec -n ${NRANKS} -ppn ${NRANKS_PER_NODE} -l --line-buffer --cpu-bind ${CPU_AFFINITY} \
+    python pytorch_ddp_large_tensors.py
+
+echo "Tensor dimension tests completed"
